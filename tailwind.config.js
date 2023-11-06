b77b58cec631335502f8d468ff180065b6954e89
mohabash011 <mohabashuni@gmail.com>
2023-11-06 03:20:51 +0300

    pushed first virsion of version of index and main css

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      colors: {
+        'custom-gray': 'rgb(243,244,246)'
+      },
+    },
+  },
+  plugins: [],
+}
