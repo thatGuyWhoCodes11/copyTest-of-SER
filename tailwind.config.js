6f566fa2d211cf5f369e40dc1a131219bc0ecda2
Abdulmalik Alamri <owu1xr@gmail.com>
2023-11-09 03:07:18 +0300

    -added phone input -added forms plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./*.html'],
+  content: ["./*.html"],
   theme: {
     extend: {
       colors: {
-        'custom-gray': 'rgb(243,244,246)'
+        "custom-gray": "rgb(243,244,246)",
       },
     },
   },
-  plugins: [],
-}
+  plugins: [require("@tailwindcss/forms")],
+};
